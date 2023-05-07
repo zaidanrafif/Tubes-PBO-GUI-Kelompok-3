465152180161dba812fa6e02e12ffa139ccc9bab
Regyyyy <regyrahman@gmail.com>
2023-05-07 18:04:18 +0700

    First commit
    Program kosong

---
+++ PerpustakaanKampus/src/main/java/com/mycompany/perpustakaankampus/PerpustakaanKampus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perpustakaankampus;
+
+/**
+ *
+ * @author User
+ */
+public class PerpustakaanKampus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
